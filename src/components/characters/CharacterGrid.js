fc20f33da2eff044d63a351a6395299eb8cd7ffd
Bahadou-Badr <badrbaha.bad@gmail.com>
2021-03-15 21:54:16 +0100

    fetch items

---
+++ src/components/characters/CharacterGrid.js
@@ -0,0 +1,15 @@
+import CharacterItem from './CharacterItem'
+
+const CharacterGrid = ({ items, isLoading }) => {
+    return ( 
+        isLoading ? (<div>Loading...</div>) : (
+            <section className='cards'>
+                {items.map(item => (
+                    <CharacterItem key={item.char_id} item={item}></CharacterItem>
+                ))}
+            </section>
+        )
+     );
+}
+ 
+export default CharacterGrid;
